a85e3d6c3ce0e0f1d77be3eacc8f907d864cef33
sam8gupta <40606070+sam8gupta@users.noreply.github.com>
2018-07-08 15:19:14 +0530

    Update answers.py

--- answers.py
+++ answers.py
@@ -2,71 +2,50 @@
 import psycopg2
 
 
-def get_popular_articles(number):
+def get_popular_articles(number, c):
     """Print most popular articles from the 'database', """
     """most recent first and 'number' in total."""
-    try:
-        db = psycopg2.connect(database="news")
-        c = db.cursor()
-        c.execute(
-            "select D.title, count(*) as views "
-            "from vw_ArticleDetails as D, vw_ValidRequests as V "
-            "where D.location = V.path "
-            "group by D.title "
-            "order by 2 desc "
-            "limit (%s);", (number,)
-            )
-        rows = c.fetchall()
-        for row in rows:
-            print('"{0}" -- {1} views'.format(row[0], row[1]))
-
-        db.close()
-    except:
-        print ("Unable to connect to the database")
+    c.execute(
+        "select D.title, count(*) as views "
+        "from vw_ArticleDetails as D, vw_ValidRequests as V "
+        "where D.location = V.path "
+        "group by D.title "
+        "order by 2 desc "
+        "limit (%s);", (number,)
+        )
+    rows = c.fetchall()
+    for row in rows:
+        print('"{0}" -- {1} views'.format(row[0], row[1]))
 
 
-def get_popular_authors():
+def get_popular_authors(c):
     """Print most popular authors from the 'database',
     most recent first and 'number' in total."""
-    try:
-        db = psycopg2.connect(database="news")
-        c = db.cursor()
-        c.execute(
-            "select A.name, count(*) as views "
-            "from vw_ArticleDetails as D, vw_ValidRequests as V, authors as A "
-            "where D.location = V.path and D.author = A.id "
-            "group by A.name "
-            "order by 2 desc;"
-            )
-        rows = c.fetchall()
-        for row in rows:
-            print('{0} -- {1} views'.format(row[0], row[1]))
-
-        db.close()
-    except:
-        print ("Unable to connect to the database")
+    c.execute(
+        "select A.name, count(*) as views "
+        "from vw_ArticleDetails as D, vw_ValidRequests as V, authors as A "
+        "where D.location = V.path and D.author = A.id "
+        "group by A.name "
+        "order by 2 desc;"
+        )
+    rows = c.fetchall()
+    for row in rows:
+        print('{0} -- {1} views'.format(row[0], row[1]))
 
 
-def get_error_days():
+def get_error_days(c):
     """Print the days from the 'database'
     when more than 1% of requests lead to errors,
     with the error percentage."""
-    try:
-        db = psycopg2.connect(database="news")
-        c = db.cursor()
-        c.execute(
-            "select A.date, round ((B.count*100)/A.count::DECIMAL, 2) as error"
-            " from vw_Requests as A, vw_Requests as B"
-            " where A.status = '200 OK' and B.status != '200 OK'"
-            " and A.date=B.date and B.count*100>A.count;"
-            )
-        rows = c.fetchall()
-        for row in rows:
-            print('{0} -- {1}% errors'.format(row[0], row[1]))
-
-        db.close()
-    except:
-        print ("Unable to connect to the database")
+    c.execute(
+        "select A.date, round ((B.count*100)/A.count::DECIMAL, 2) as error"
+        " from vw_Requests as A, vw_Requests as B"
+        " where A.status = '200 OK' and B.status != '200 OK'"
+        " and A.date=B.date and B.count*100>A.count;"
+        )
+    rows = c.fetchall()
+    for row in rows:
+        print('{0} -- {1}% errors'.format(row[0], row[1]))
 
 
 print(
@@ -74,13 +53,23 @@ print(
     "\nLet us grab some answers from the tool!!!\n"
     "\n1. What are the most popular three articles of all time?\n")
 
-print("The most popular three articles of all time are:")
-get_popular_articles(3)
+try:
+    # connecting to database to execute the queries
+    db = psycopg2.connect(database="news")
+    cursor = db.cursor()
+    print("The most popular three articles of all time are:")
+    get_popular_articles(3, cursor)
 
-print("\n2. Who are the most popular article authors of all time?\n")
-print("The most popular article authors of all time are:")
-get_popular_authors()
+    print("\n2. Who are the most popular article authors of all time?\n")
+    print("The most popular article authors of all time are:")
+    get_popular_authors(cursor)
 
-print("\n3. On which days did more than 1% of requests lead to errors?\n")
-print("The days on which more than 1% of requests lead to errors are:")
-get_error_days()
+    print("\n3. On which days did more than 1% of requests lead to errors?\n")
+    print("The days on which more than 1% of requests lead to errors are:")
+    get_error_days(cursor)
+
+    db.close()
+
+
+except:
+    print ("Unable to connect to the database")
